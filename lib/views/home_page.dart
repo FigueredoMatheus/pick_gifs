7d282c77e568849afca65ebda0ac64b58fd6400d
MatheusF <matheusfigueredob@gmail.com>
2022-03-23 15:05:58 -0300

    Cria textField na home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -14,10 +14,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black54,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Image.asset(
           'assets/PoweredBy_640_Horizontal_Light-Backgrounds_With_Logo.gif',
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: const [
+            TextField(
+              cursorColor: Colors.white,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'Procurar por',
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.teal),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 1, color: Colors.black),
+                ),
+                focusColor: Colors.white,
+                filled: true,
+                fillColor: Colors.black26,
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
