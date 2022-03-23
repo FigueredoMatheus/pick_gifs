ca8e814af198dfcaf11a9040d80912974735149f
MatheusF <matheusfigueredob@gmail.com>
2022-03-23 13:19:29 -0300

    Cria file gifs_api

---
+++ lib/gifs_api.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class GifsAPI {
+  static Future<Map> getSearchGifs(String requestedWord, int offset) async {
+    final url = Uri.parse(
+        'https://api.giphy.com/v1/gifs/search?api_key=II90afVadpAi9LQEWjPpGhOR47VpzExl&q=$requestedWord&limit=25&offset=$offset&rating=g&lang=pt');
+
+    http.Response response = await http.get(url);
+
+    return json.decode(response.body);
+  }
+
+  static getTrendingGifs() async {
+    final url = Uri.parse(
+        'https://api.giphy.com/v1/gifs/trending?api_key=II90afVadpAi9LQEWjPpGhOR47VpzExl&limit=50&rating=g');
+
+    http.Response response = await http.get(url);
+
+    return json.decode(response.body);
+  }
+}
