b02fb5831a33d11521c2f2b7f72830e0aab53549
MatheusF <matheusfigueredob@gmail.com>
2022-03-25 08:46:31 -0300

    Altera do statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gifs_search/app.dart';
 
 void main() {
@@ -9,6 +10,19 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.black, // cor da barra superior
+        statusBarIconBrightness: Brightness.light, // ícones dabarra superior
+        systemNavigationBarColor:
+            Color.fromARGB(232, 0, 0, 0), // cor da barra inferior
+        systemNavigationBarIconBrightness:
+            Brightness.light, // ícones da barra inferior
+      ),
+    );
     return MaterialApp(
       title: 'Buscador de Gifs',
       debugShowCheckedModeBanner: false,
